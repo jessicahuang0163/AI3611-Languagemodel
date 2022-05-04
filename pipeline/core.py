df61ca0fff64592ad3e7ade2c5e6d3075d08bc30
Jessica-legend <legend0018@sjtu.edu.cn>
2022-05-04 10:15:05 +0000

    Code format

---
+++ pipeline/core.py
@@ -0,0 +1,66 @@
+import os
+import torch
+import model
+
+# Starting from sequential data, batchify arranges the dataset into columns.
+# For instance, with the alphabet as the sequence and batch size 4, we'd get
+# ┌ a g m s ┐
+# │ b h n t │
+# │ c i o u │
+# │ d j p v │
+# │ e k q w │
+# └ f l r x ┘.
+# These columns are treated as independent by the model, which means that the
+# dependence of e. g. 'g' on 'f' can not be learned, but allows more efficient
+# batch processing.
+
+
+def batchify(data, bsz, device):
+    # Work out how cleanly we can divide the dataset into bsz parts.
+    nbatch = data.size(0) // bsz
+    # Trim off any extra elements that wouldn't cleanly fit (remainders).
+    data = data.narrow(0, 0, nbatch * bsz)
+    # Evenly divide the data across the bsz batches.
+    data = data.view(bsz, -1).t().contiguous()
+    return data.to(device)
+
+
+def repackage_hidden(h):
+    """Wraps hidden states in new Tensors, to detach them from their history."""
+
+    if isinstance(h, torch.Tensor):
+        return h.detach()
+    else:
+        return tuple(repackage_hidden(v) for v in h)
+
+
+# get_batch subdivides the source data into chunks of length args.bptt.
+# If source is equal to the example output of the batchify function, with
+# a bptt-limit of 2, we'd get the following two Variables for i = 0:
+# ┌ a g m s ┐ ┌ b h n t ┐
+# └ b h n t ┘ └ c i o u ┘
+# Note that despite the name of the function, the subdivison of data is not
+# done along the batch dimension (i.e. dimension 1), since that was handled
+# by the batchify function. The chunks are along dimension 0, corresponding
+# to the seq_len dimension in the LSTM.
+
+
+def get_batch(exp_specs, source, i):
+    seq_len = min(exp_specs["bptt"], len(source) - 1 - i)
+    data = source[i : i + seq_len]
+    target = source[i + 1 : i + 1 + seq_len].view(-1)
+    return data, target
+
+
+def export_onnx(path, batch_size, seq_len, device='cuda'):
+    print(
+        "The model is also exported in ONNX format at {}.".format(
+            os.path.realpath(path)
+        )
+    )
+    model.eval()
+    dummy_input = (
+        torch.LongTensor(seq_len * batch_size).zero_().view(-1, batch_size).to(device)
+    )
+    hidden = model.init_hidden(batch_size)
+    torch.onnx.export(model, (dummy_input, hidden), path)
---
+++ pipeline/pipeline.py
@@ -0,0 +1,85 @@
+import time
+import torch
+import torch.nn as nn
+import math
+from pipeline import core
+
+def train(model, exp_specs, corpus, epoch, device='cuda'):
+    # Turn on training mode which enables dropout.
+    model.train()
+    total_loss = 0.0
+    start_time = time.time()
+    train_data = core.batchify(corpus.train, exp_specs["batch_size"], device)
+    ntokens = len(corpus.dictionary)
+    criterion = nn.NLLLoss()
+    if exp_specs["model_name"] != "Transformer":
+        hidden = model.init_hidden(exp_specs["batch_size"])
+    for batch, i in enumerate(range(0, train_data.size(0) - 1, exp_specs["bptt"])):
+        data, targets = core.get_batch(exp_specs, train_data, i)
+        # Starting each batch, we detach the hidden state from how it was previously produced.
+        # If we didn't, the model would try backpropagating all the way to start of the dataset.
+        model.zero_grad()
+        if exp_specs["model_name"] == "Transformer":
+            output = model(data)
+            output = output.view(-1, ntokens)
+        else:
+            hidden = core.repackage_hidden(hidden)
+            output, hidden = model(data, hidden)
+        loss = criterion(output, targets)
+        loss.backward()
+
+        # `clip_grad_norm` helps prevent the exploding gradient problem in RNNs / LSTMs.
+        torch.nn.utils.clip_grad_norm_(model.parameters(), exp_specs["clip"])
+        for p in model.parameters():
+            p.data.add_(p.grad, alpha=-exp_specs["lr"])
+
+        total_loss += loss.item()
+
+        if batch % exp_specs["log_interval"] == 0 and batch > 0:
+            cur_loss = total_loss / exp_specs["log_interval"]
+            elapsed = time.time() - start_time
+            print(
+                "| epoch {:3d} | {:5d}/{:5d} batches | lr {:02.2f} | ms/batch {:5.2f} | "
+                "loss {:5.2f} | ppl {:8.2f}".format(
+                    epoch,
+                    batch,
+                    len(train_data) // exp_specs["bptt"],
+                    exp_specs["lr"],
+                    elapsed * 1000 / exp_specs["log_interval"],
+                    cur_loss,
+                    math.exp(cur_loss),
+                )
+            )
+            total_loss = 0
+            start_time = time.time()
+        if exp_specs["dry_run"]:
+            break
+
+
+def evaluate(model, exp_specs, corpus, mode, device='cuda'):
+    # Turn on evaluation mode which disables dropout.
+    model.eval()
+    total_loss = 0.0
+    eval_batch_size = 10
+    criterion = nn.NLLLoss()
+    ntokens = len(corpus.dictionary)
+    if mode == "val":
+        data_source = core.batchify(corpus.valid, eval_batch_size, device)
+    elif mode == "test":
+        data_source = core.batchify(corpus.test, eval_batch_size, device)
+    else:
+        raise ValueError("mode must be either val or test")
+
+    if exp_specs["model_name"] != "Transformer":
+        hidden = model.init_hidden(eval_batch_size)
+    with torch.no_grad():
+        for i in range(0, data_source.size(0) - 1, exp_specs["bptt"]):
+            data, targets = core.get_batch(exp_specs, data_source, i)
+            if exp_specs["model_name"] == "Transformer":
+                output = model(data)
+                output = output.view(-1, ntokens)
+            else:
+                output, hidden = model(data, hidden)
+                hidden = core.repackage_hidden(hidden)
+            total_loss += len(data) * criterion(output, targets).item()
+    return total_loss / (len(data_source) - 1)
